2cbfeb37f6fd36fc6e5bbbae7a9788ca8595c69d
Cristian Peña <48634410+cristianpenia@users.noreply.github.com>
2024-07-10 17:19:36 -0600

    [add] class

---
+++ src/Class.kt
@@ -0,0 +1,34 @@
+class Personas {
+    // atributos (variables)
+    var nombre =""
+    var edad = 0
+
+    // constructor secundario
+    constructor(nombre:String, edad:Int)  {
+        this.nombre = nombre
+        this.edad = edad
+    }
+
+    // metodos (funciones)
+    fun hola() {
+        println("Hello world, my name is $nombre and i am $edad years")
+    }
+}
+
+class Usuarios(private var name: String, private  var email: String){
+    fun login() {
+        println("Login con user: $name, email: $email")
+    }
+}
+
+
+fun main() {
+    val personas = Personas(nombre = "Cristian", edad = 26)
+    println(personas.nombre)
+    println(personas.edad)
+    println(personas.hola())
+
+    val user = Usuarios(name = "Rufus", email = "rufus_king@gmail.com")
+    println(user.login())
+}
+
